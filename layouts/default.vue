c94cb1ab9d47081c0911ca1bc51d6e2acd4665cc
marinofranz <mvbjarnasynir@gmail.com>
2021-01-04 21:40:12 +0000

    :tada: Just the base, really

---
+++ layouts/default.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      dense
+      hide-on-scroll
+    >
+      <v-container>
+        <div class="title">
+          <nuxt-link to="/">Marino Franz</nuxt-link>
+        </div>
+        <v-spacer></v-spacer>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <v-container class="page-container">
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>{{ title }} — {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<style scoped>
+  .page-container {
+    margin-top: 20px;
+  }
+
+  .title a {
+    color: #fff;
+    cursor: pointer;
+    text-decoration: none;
+    transition: opacity 175ms;
+  }
+
+  .title a:hover {
+    opacity: 0.7;
+  }
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Marino Franz'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-alert
+      outlined
+      type="warning"
+      border="left"
+    >
+      This website is still under development, please report bugs to Marino over on his <strong class="cursor-pointer" @click.stop="dialog = true">Discord</strong>
+    </v-alert>
+
+    <v-dialog
+    v-model="dialog"
+    max-width="290"
+    >
+      <v-card dark>
+        <v-card-title>
+          Discord Tag
+        </v-card-title>
+        <v-card-text>
+          If you have any questions or concerns, please direct message
+          the following Discord user:
+          <br>
+          <br>
+          <strong>{{ discordTag }}</strong>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<style scoped>
+  .cursor-pointer {
+    cursor: pointer;
+  }
+</style>
+
+<script>
+export default {
+  data(){
+    return {
+      dialog: false,
+      discordTag: "Marino#0001"
+    }
+  }
+}
+</script>
