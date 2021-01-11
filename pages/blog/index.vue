06933478b3a2465843c2f65d14acd90022fc737d
Marino <48334228+marinofranz@users.noreply.github.com>
2021-01-11 10:48:26 +0000

    Parameters change

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -46,7 +46,7 @@
 <script>
 export default {
   async asyncData({ $content, params }){
-    const articles = await $content();
+    const articles = await $content("/");
       .sortBy("createdAt", "asc")
       .fetch();
 
